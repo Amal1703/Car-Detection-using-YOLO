c519d06d05e1b05c2620894ca0a836f99a67052f
Amal <77744121+Amal1703@users.noreply.github.com>
2026-04-15 15:39:16 +0200

    Add search_subfolder_contain_files_txt function to search for subfolders containing files.txt

---
+++ custom/search_subfolder_contain_files_txt_function.py
@@ -0,0 +1,17 @@
+# This function finds the subfolder that contains the files.txt (corresponding to YOLO annotations)
+
+# path_main_folder: path of the main folder
+def search_subfolder_contain_files_txt (path_main_folder) :
+
+    base_path = Path(chemin)
+
+    for subfolder in base_path.rglob("*"):
+        if subfolder.is_dir():
+            files = list(subfolder.iterdir())
+
+            # check: subfolder is not empty and all files are .txt
+            if files and all(f.is_file() and f.suffix == ".txt" for f in files):
+                print(subfolder)
+                break
+                
+    return str(subfolder)

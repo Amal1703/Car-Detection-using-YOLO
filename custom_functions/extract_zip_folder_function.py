abca906c466525943e350959027a0e835652a51e
Amal <77744121+Amal1703@users.noreply.github.com>
2026-04-16 10:33:14 +0200

    Update extract_zip_folder_function.py

--- custom_functions/extract_zip_folder_function.py
+++ custom_functions/extract_zip_folder_function.py
@@ -1,9 +1,9 @@
 # This function extracts a zip folder
 
-import zipfile
-
 # zip_path: path to the zip folder
 # extract_to: folder where files will be extracted
+
+import zipfile
 
 def extract_zip_folder (zip_path, extract_to) :
  with zipfile.ZipFile(zip_path, 'r') as zip_ref:
